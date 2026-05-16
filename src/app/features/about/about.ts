733186b4cb7f9175f9db9ca0a241cefc4dfaa052
RyanSoar3s <ryansoares.dev@gmail.com>
2026-05-16 18:01:48 -0300

    feat: add host id to About component for improved accessibility

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
+  host: { id: 'inicio' },
   imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css',
